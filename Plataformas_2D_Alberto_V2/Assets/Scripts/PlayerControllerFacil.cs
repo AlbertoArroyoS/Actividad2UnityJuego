df943e1a696526bad7eadcb8ed119d9cf6f7a4f2
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-02-21 15:55:54 +0100

    Ejecutables juego

--- Plataformas_2D_Alberto_V2/Assets/Scripts/PlayerControllerFacil.cs
+++ Plataformas_2D_Alberto_V2/Assets/Scripts/PlayerControllerFacil.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.EditorTools;
+//using UnityEditor.EditorTools;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
